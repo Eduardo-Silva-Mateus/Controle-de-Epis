8ad8e269fbf3f7d1f6274873af50b59edef5d2ee
Eduardo Mateus da Silva <eduardomateussilva48@gmail.com>
2024-11-15 00:43:04 -0300

    Começo do trabalho da SA

---
+++ controledeepis/src/main/java/com/antiia/controledeepis/ControledeepisApplication.java
@@ -0,0 +1,13 @@
+package com.antiia.controledeepis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControledeepisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControledeepisApplication.class, args);
+	}
+
+}
---
+++ controledeepis/src/test/java/com/antiia/controledeepis/ControledeepisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.antiia.controledeepis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControledeepisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
